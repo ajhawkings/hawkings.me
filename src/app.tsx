a3754a23571549f76f6de742ed257db3bc093588
Angus <70203823+ajhawkings@users.noreply.github.com>
2022-07-04 16:18:11 +0100

    Redo routing and imports (#9)
    * Redo routing and imports
    
    * Format code with prettier
    
    * Add import note
    
    Co-authored-by: ajhawkings <ajhawkings@users.noreply.github.com>

--- src/app.tsx
+++ src/app.tsx
@@ -1,23 +1,33 @@
-import { useRoutes, useLocation } from 'solid-app-router'
+import { createEffect, lazy } from 'solid-js'
 import { routes } from './routes'
 
+import { Routes, Route } from 'solid-app-router'
 import NavBar from './components/NavBar'
 
 import type { Component } from 'solid-js'
 
 const App: Component = () => {
-  const Route = useRoutes(routes)
-
-  const location = useLocation()
-  const path = location.pathname
-  const title = routes.find((route) => route.path === path)?.name
+  createEffect(() => {
+    const route = routes.find((r) => r.path === window.location.pathname)
+    document.title = route?.title
+      ? `hawkings.me | ${route.title}`
+      : 'hawkings.me'
+  })
 
   return (
     <>
       <NavBar />
       <main>
-        <h1>{title}</h1>
-        <Route />
+        <Routes>
+          {routes.map((route) => (
+            <Route
+              path={route.path}
+              component={lazy(
+                () => import(`./${route.folder}/${route.component}`)
+              )}
+            />
+          ))}
+        </Routes>
       </main>
     </>
   )
