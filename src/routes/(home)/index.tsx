4b643d249bbab1a4129677c0030c6820cecaeeb4
ajhawkings <ajhawkings@users.noreply.github.com>
2022-12-31 19:49:33 +0000

    Format code with prettier

--- src/routes/(home)/index.tsx
+++ src/routes/(home)/index.tsx
@@ -8,8 +8,15 @@ export default function Home() {
       <Title>Home | hawkings.me</Title>
       <div class={styles.main}>
         <h2>Welcome to my website - hawkings.me</h2>
-        <p>As you can probably see, it is under construction. CSS is a luxury, anyway...</p>
-        <p>In the meantime please check out my <a href="https://github.com/ajhawkings">GitHub profile</a>. It has info on what I'm up to!</p>
+        <p>
+          As you can probably see, it is under construction. CSS is a luxury,
+          anyway...
+        </p>
+        <p>
+          In the meantime please check out my{' '}
+          <a href="https://github.com/ajhawkings">GitHub profile</a>. It has
+          info on what I'm up to!
+        </p>
       </div>
     </>
   )
