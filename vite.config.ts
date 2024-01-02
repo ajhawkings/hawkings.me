517c098ee6ff40b8827f9ed0a010921535946699
ajhawkings <ajhawkings@users.noreply.github.com>
2024-01-02 23:09:28 +0000

    Format code with prettier

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from '@solidjs/start/config'
 export default defineConfig({
   start: {
     server: {
-      preset: 'static'
-    }
-  }
-});
+      preset: 'static',
+    },
+  },
+})
